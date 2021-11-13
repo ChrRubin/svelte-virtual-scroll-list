4f7045ac4055c2ce4eb9b01a4a99f1fad2051201
ChrRubin <20059164+ChrRubin@users.noreply.github.com>
2021-11-13 17:34:40 +0800

    Optional props, add docs

--- src/VirtualScroll.svelte.d.ts
+++ src/VirtualScroll.svelte.d.ts
@@ -2,16 +2,26 @@ import { SvelteComponentTyped } from "svelte";
 import type { Range } from "./virtual"
 
 export interface VirtualScrollProps<T> {
+  /** Unique key for getting data from `data` */
   key: string;
+  /** Source for list */
   data: T[];
-  keeps: number;
-  estimateSize: number;
-  isHorizontal: boolean;
-  start: number;
-  offset: number;
-  pageMode: boolean;
-  topThreshold: number;
-  bottomThreshold: number;
+  /** Count of rendered items */
+  keeps?: number;
+  /** Estimate size of each item, needs for smooth scrollbar */
+  estimateSize?: number;
+  /** Scroll direction */
+  isHorizontal?: boolean;
+  /** scroll position start index */
+  start?: number;
+  /** scroll position offset */
+  offset?: number;
+  /** Let virtual list using global document to scroll through the list */
+  pageMode?: boolean;
+  /** The threshold to emit `top` event, attention to multiple calls. */
+  topThreshold?: number;
+  /** The threshold to emit `bottom` event, attention to multiple calls. */
+  bottomThreshold?: number;
 }
 
 export interface VirtualScrollEvents {
